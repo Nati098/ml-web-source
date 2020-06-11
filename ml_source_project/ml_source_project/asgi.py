622c6da671bac1dc831ef9c8074c09bdbcb03e7d
Bogatyreva Anastasia <bogatyreva@bpcbt.com>
2020-06-11 13:40:38 +0300

    Initial commit [2]

--- ml_source_project/ml_source_project/asgi.py
+++ ml_source_project/ml_source_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ml_web_source project.
+ASGI config for ml_source_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ml_web_source.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ml_source_project.settings')
 
 application = get_asgi_application()
